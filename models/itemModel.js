b5f209179f30a7f716a18179c94439e1d257e67f
n1steka <mongolzkarigun@gmail.com>
2024-05-28 17:33:47 +0800

    xD

--- models/itemModel.js
+++ models/itemModel.js
@@ -11,6 +11,10 @@ const itemSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Service",
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Item = mongoose.model("Item", itemSchema);
