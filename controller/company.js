af19e294cfa0dc74755d2c5256f3cd742572d8dc
Vnenhvv <vnenhvv0731@gmail.com>
2024-11-13 13:18:21 +0800

    populate category

--- controller/company.js
+++ controller/company.js
@@ -12,7 +12,8 @@ exports.getAll = asyncHandler(async (req, res, next) => {
     const categories = await Model.find()
       .populate("area")
       .populate("district")
-      .populate("subDistrict");
+      .populate("subDistrict")
+      .populate('category');
 
     const allUser = await Fav.find({ user: req.userId });
 
