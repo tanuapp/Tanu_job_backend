5a509e627a879788e0629e2bbd689bc6d5763aec
tanuapp <developer.tanusoft@gmail.com>
2025-10-09 16:57:19 +0800

    a

--- firebaseInit.js
+++ firebaseInit.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccountwp = require("./key firebase.json");
+const serviceAccountwp = require("./tanu-app-928a8-firebase-adminsdk-mrr1i-0bf2b2ad4a.json");
 
 const initFirebase = async () => {
   try {
